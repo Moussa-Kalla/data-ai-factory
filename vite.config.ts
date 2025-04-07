968def446aa21f921bdf67416182bd185d58e591
Moussa-Kalla <moussa.kalla.am@gmail.com>
2025-04-08 00:01:41 +0200

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/BayanAI/', // ← très important pour GitHub Pages
+  build: {
+    outDir: 'docs'   // ← pour générer le dossier docs/
+  }
 });
